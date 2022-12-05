2891fee8ce590fe4a4c731aa938eb03507ef55a1
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-12-05 16:41:46 +0000

    edited vercel deployment

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { getUsers, getStations,getUser,getStation} from './src/firestoreFunctions.js';
+
+const app = express();
+app.use(express.json({ extended: false }));
+
+
+app.use("/", (req, res) => {
+    res.send('Hello!!! Check out the other endpoints')
+});
+
+app.use("/test", (req, res) => {
+    res.send('Hello!!! Check out the other endpoints')
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
