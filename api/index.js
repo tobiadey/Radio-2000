4072ca1a349703a25a8a787000ec43e0dd8fb760
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-12-05 16:51:02 +0000

    edited vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,25 +1,57 @@
 import express from 'express';
 import { getUsers, getStations,getUser,getStation} from '../src/firestoreFunctions.js';
 
-const app = express()
-const port = 3000
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
 
 
-app.get('/', (req, res) => {
-  res.send('Hello!!! Check out the other endpoints')
-})
-
-
-app.get('/users', getUsers);
-app.get('/stations', getStations);
-app.get('/user/:name',getUser )
-app.get('/station/:name',getStation )
 
 
 
-app.listen(port, () => {
-  console.log(`🚀 Server ready at http://localhost:${port}`);
-})
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import { getUsers, getStations,getUser,getStation} from '../src/firestoreFunctions.js';
+
+// const app = express()
+// const port = 3000
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello!!! Check out the other endpoints')
+// })
+
+
+// app.get('/users', getUsers);
+// app.get('/stations', getStations);
+// app.get('/user/:name',getUser )
+// app.get('/station/:name',getStation )
+
+
+
+// app.listen(port, () => {
+//   console.log(`🚀 Server ready at http://localhost:${port}`);
+// })
 
 
 
--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 import express from 'express';
 import { getUsers, getStations,getUser,getStation} from './src/firestoreFunctions.js';
 
-const app = express();
-// app.use(express.json({ extended: false }));
+
+const app = express()
+const port = 3000
 
 
-// app.get("/", (req, res) => {
-//     res.send('Hello!!! Check out the other endpoints')
-// });
+app.get('/', (req, res) => {
+  res.send('Hello!!! Check out the other endpoints')
+})
 
-// app.get("/test", (req, res) => {
-//     res.send('Hello!!! Check out the other endpoints')
-//   });
+app.get('/users', getUsers);
+app.get('/stations', getStations);
+app.get('/user/:name',getUser )
+app.get('/station/:name',getStation )
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}`);
+})
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import { getUsers, getStations,getUser,getStation} from './src/firestoreFunctions.js';
+
+// const app = express();
+// // app.use(express.json({ extended: false }));
+
+
+// // app.get("/", (req, res) => {
+// //     res.send('Hello!!! Check out the other endpoints')
+// // });
+
+// // app.get("/test", (req, res) => {
+// //     res.send('Hello!!! Check out the other endpoints')
+// //   });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
