0e8c058efbdfab2a4be3d0a981a6e6c24b1f742e
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-12-05 15:44:26 +0000

    edited firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,25 +6,17 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 
-// const firebaseConfig = {
-//     apiKey: process.env.APIKEY,
-//     authDomain: process.env.AUTHDOMAIN,
-//     projectId: process.env.PROJECTID,
-//     storageBucket: process.env.STORAGEBUCKET,
-//     messagingSenderId: process.env.MESSAGINGSENDERID,
-//     appId: process.env.APPID,
-//     measurementId: process.env.MEASUREMENTID
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyB8xjSwxm-0wJptZDayGeE0Qt9JrsNkwwg",
-    authDomain: "radio-stations-ce7cc.firebaseapp.com",
-    projectId: "radio-stations-ce7cc",
-    storageBucket: "radio-stations-ce7cc.appspot.com",
-    messagingSenderId: "73665985644",
-    appId: "1:73665985644:web:d649813e866c080b68ac49",
-    measurementId: "G-NFQ6RJ4WC9"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
 };
+
+
 
 
 // Initialize Firebase
