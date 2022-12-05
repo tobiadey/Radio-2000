91ff1aad44e2ae64668ad9d61b036d82d30089a4
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-12-05 16:45:28 +0000

    edited vercel deployment

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from 'express';
 import { getUsers, getStations,getUser,getStation} from './src/firestoreFunctions.js';
 
 const app = express();
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 
-app.get("/", (req, res) => {
-    res.send('Hello!!! Check out the other endpoints')
-});
+// app.get("/", (req, res) => {
+//     res.send('Hello!!! Check out the other endpoints')
+// });
 
-app.get("/test", (req, res) => {
-    res.send('Hello!!! Check out the other endpoints')
-  });
+// app.get("/test", (req, res) => {
+//     res.send('Hello!!! Check out the other endpoints')
+//   });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
